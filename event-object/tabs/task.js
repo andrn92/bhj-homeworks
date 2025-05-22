a186e30bbf48c13567b5c17874c28dc7e6ac5d1f
andrn92 <georgkartashov@yandex.ru>
2025-05-22 18:31:45 +0300

    last change

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -9,9 +9,7 @@ for (let i=0; i < tabNavigation.children.length; i += 1) {
   tabNavigation.children[i].onclick = function(event) {
     deleteTabs(tabNavigation.children, 'tab_active')
     event.target.classList.add('tab_active')
-    for (let tabContent of tabContents.children) {
-      tabContent.classList.remove('tab__content_active')
-    }
+    deleteTabs(tabContents.children, 'tab__content_active')
     tabContents.children[i].classList.add('tab__content_active')
   }
 }
