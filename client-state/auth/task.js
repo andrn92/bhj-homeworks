8c715b15269b41a3f531cca7d772800278b31110
andrn92 <georgkartashov@yandex.ru>
2025-05-28 01:20:36 +0300

    last change

---
+++ client-state/auth/task.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("signin__form")
+const button = document.getElementById('signin__btn')
+const welcome = document.getElementById('welcome')
+const userId = document.getElementById('user_id')
+button.setAttribute('type', 'submit')
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+  const login = form.elements.login.value
+  const password = form.elements.password.value
+  fetch('https://students.netoservices.ru/nestjs-backend/auth', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ login, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      welcome.classList.add('welcome_active')
+      userId.textContent = data['user_id']
+      localStorage.setItem('user', JSON.stringify(data.user))
+    } else {
+      alert("Login or password is incorrect")
+    }
+  })
+  .catch(error => {
+    console.error('Error sending data:', error)
+  })
+})    
