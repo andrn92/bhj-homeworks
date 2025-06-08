c711b869988c07379288c0e2224a37e35dcc775e
andrn92 <georgkartashov@yandex.ru>
2025-06-09 01:20:17 +0300

    last change

--- client-state/auth/task_old.js
+++ client-state/auth/task_old.js
@@ -1,10 +1,8 @@
-const signin = document.getElementById("signin")
-signin.classList.add('signin_active')
 const form = document.getElementById("signin__form")
 const button = document.getElementById('signin__btn')
 const welcome = document.getElementById('welcome')
 const userId = document.getElementById('user_id')
-button.addEventListener("click", function(event) {
+form.addEventListener("submit", function(event) {
   event.preventDefault()
   const login = form.elements.login.value
   const password = form.elements.password.value
@@ -19,15 +17,15 @@ button.addEventListener("click", function(event) {
   .then(response => response.json())
   .then(data => {
     if (data.success) {
-      let usId = localStorage.getItem('user_id')  
+      let usId = localStorage.getItem('user')  
       welcome.classList.add('welcome_active')
-      signin.classList.remove('signin_active')
       if (!usId) {
-        localStorage.setItem('user_id', data.user_id)
+        localStorage.setItem('user', data.user_id)
         userId.textContent = data.user_id
       } else {
         userId.textContent = usId
       }
+      console.log(localStorage)
     } else {
       alert("Login or password is incorrect")
     }
